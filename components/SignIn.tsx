276bdaf79ebe8e68265a134e1885bbd4637eb3b0
Захар Александрук <zaharaleksandruk@MacBook-Air-Zahar.local>
2025-11-16 13:59:50 +0200

    Add Sign In markup

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,7 +1,35 @@
-import { ScrollView, StyleSheet, Image, Text } from "react-native";
+import { Alert, ScrollView, StyleSheet, Image, Text, View } from "react-native";
 import Input from "./Input";
+import AppButton from "./AppButton";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+	email: yup.string().required("Email is required").email("Invalid email"),
+	password: yup
+		.string()
+		.required("Password is required")
+		.min(8, "Min 8 chars"),
+});
 
 const SignIn = () => {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(validationSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (data: any) => {
+		Alert.alert("Form data:", JSON.stringify(data, null, 2));
+	};
+
 	return (
 		<ScrollView
 			style={{ flex: 1, backgroundColor: "#6871EE" }}
@@ -9,6 +37,41 @@ const SignIn = () => {
 		>
 			<Image source={require("./assets/Logo.png")} style={styles.logo} />
 			<Text style={styles.welcomeText}>Welcome!</Text>
+			<View style={styles.inputContainer}>
+				<Controller
+					control={control}
+					name="email"
+					render={({ field: { onChange, onBlur, value } }) => (
+						<Input
+							label="Email"
+							onBlur={onBlur}
+							onChangeText={onChange}
+							value={value}
+							error={errors.email?.message}
+							keyboardType="email-address"
+							autoCapitalize="none"
+						/>
+					)}
+				/>
+				<Controller
+					control={control}
+					name="password"
+					render={({ field: { onChange, onBlur, value } }) => (
+						<Input
+							label="Password"
+							onBlur={onBlur}
+							onChangeText={onChange}
+							value={value}
+							error={errors.password?.message}
+							secureTextEntry={true}
+						/>
+					)}
+				/>
+			</View>
+			<View style={styles.buttonContainer}>
+				<AppButton title="Sign Up" onPress={handleSubmit(onSubmit)} />
+				<AppButton title="Go To Sign In" onPress={() => {}} />
+			</View>
 		</ScrollView>
 	);
 };
@@ -19,8 +82,17 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "#6871EE",
-		gap: 30,
 		paddingTop: 78,
+	},
+
+	inputContainer: {
+		marginTop: 101,
+		gap: 11,
+	},
+
+	buttonContainer: {
+		marginTop: 49,
+		gap: 11,
 	},
 
 	welcomeText: {
