b1c68acc4fbadd5454cefb40c0bb6c771acf5f86
Тарас Колодрібський <taras.kolodribskyi.vr.2025@lpnu.ua>
2025-11-21 15:17:09 +0200

    ffafa

--- components/SignUpLogic.tsx
+++ components/SignUpLogic.tsx
@@ -2,8 +2,7 @@ import { useSelector } from "react-redux";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import * as Keychain from "react-native-keychain";
 import { Alert, ScrollView, StyleSheet, Image, Text, View } from "react-native";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
-import { setToken } from "./tokenSlice";
+import saveToken from "./saveToken";
 
 type postRequestError = any | unknown | AxiosError;
 type axiosRegistrationResponse = any | AxiosResponse;
@@ -17,19 +16,6 @@ const signUp = async ({
 	name: string;
 	password: string;
 }) => {
-	/*await axios
-		.post("/api/auth/registration/", {
-			email,
-			name,
-			password,
-		})
-		.then(response => {
-			console.log(response);
-		})
-		.catch(e => Alert.alert(e));
-	return <></>;*/
-	const token = useAppSelector(state => state.token.value);
-	const dispatch = useAppDispatch();
 	try {
 		const res: axiosRegistrationResponse = await axios.post(
 			"localhost:3000/api/auth/registration/",
@@ -39,7 +25,7 @@ const signUp = async ({
 				password,
 			},
 		);
-		dispatch(setToken(res.token));
+		saveToken(res.token);
 	} catch (error: postRequestError) {
 		Alert.alert(error);
 	}
